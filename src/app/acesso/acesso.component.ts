76952d59c0aa11d3ecddfaecd5a7676a81c7c145
Rafael Reis <rafyangtj@gmail.com>
2023-08-03 11:40:38 -0300

    ended animation

--- src/app/acesso/acesso.component.ts
+++ src/app/acesso/acesso.component.ts
@@ -4,6 +4,7 @@ import {
   state,
   transition,
   animate,
+  keyframes,
 } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
 
@@ -28,7 +29,22 @@ import { Component, OnInit } from '@angular/core';
       state('criado', style({ opacity: 1 })),
       transition('void => criado', [
         style({ opacity: 0, transform: 'translate(50px, 0)' }),
-        animate('500ms 1s ease-in-out'),
+        animate(
+          '1.5s 0s ease-in-out',
+          keyframes([
+            style({ offset: 0.15, opacity: 1, transform: 'translateX(0)' }),
+            style({ offset: 0.8, opacity: 1, transform: 'translateX(0)' }),
+
+            style({ offset: 0.88, opacity: 1, transform: 'translateY(-10px)' }),
+            style({ offset: 0.9, opacity: 1, transform: 'translateY(10px)' }),
+            style({ offset: 0.92, opacity: 1, transform: 'translateY(-10px)' }),
+            style({ offset: 0.94, opacity: 1, transform: 'translateY(10px)' }),
+            style({ offset: 0.96, opacity: 1, transform: 'translateY(-10px)' }),
+            style({ offset: 0.98, opacity: 1, transform: 'translateY(10px)' }),
+
+            style({ offset: 1, opacity: 1, transform: 'translateX(0)' }),
+          ])
+        ),
       ]),
     ]),
   ],
@@ -45,5 +61,13 @@ export class AcessoComponent implements OnInit {
 
   public exibirPainel(event: string) {
     this.cadastro = event === 'cadastro' ? true : false;
+  }
+
+  public inicioAnimacao(): void {
+    console.log('inicio da animação');
+  }
+
+  public fimAnimacao(): void {
+    console.log('fim da animação');
   }
 }
